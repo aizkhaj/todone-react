ccdffc3e03659fc1281bd06703a600e4f9cffbc0
Aizaz Khaja <aizazkj@gmail.com>
2017-10-04 23:06:47 -0400

    can view tasks, but only with static data. need to add ability to connect live data next

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App/App.css';
+import List from '../List/List';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/List/List.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Item from '../Item/Item';
+
+class List extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      items: [
+        {
+          title: 'Walk the cat',
+          completed: false
+        },
+        {
+          title: "Find a cat",
+          completed: true
+        }
+      ]
+    };
+  }
+
+  toggleComplete(index) {
+    const items = this.state.items.slice();
+    const item = items[index];
+    item.completed = item.completed ? false : true;
+    this.setState({ items: items });
+  }
+
+  render() {
+    return (
+      <ul>
+        {
+          this.state.items.map((item, index) => (
+            <Item 
+              key={index} 
+              title={item.title} 
+              completed={item.completed} 
+              toggleComplete={() => {
+              this.toggleComplete(index);
+            }} />
+          ))
+        }
+      </ul>
+    )
+  }
+}
+
+export default List;
